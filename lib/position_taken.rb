19a9090c57e7408dbfde51e9d5f29a3c6392e3b9
kaaatom <kaaatom@mail.ru>
2016-04-22 22:47:02 +0700

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "], position)
   board[position] == ""
-  board[position] == "X" or board[position] == "O"
+  board[position] == "X" || board[position] == "O"
 end
